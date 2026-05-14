2dd2e3e25bf78a2a7a5babd9720bf93c15dcd2d6
unknown <2jd@e041edu.local>
2026-05-14 10:36:16 -0300

    ńovissimas alterações

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,29 +2,78 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    //fR = facing right
+    //tS = the scale
     private Rigidbody2D rb;
     private float horizontal;
+    private float vertical;
 
-    [SerializeField] private float movementSpeed;
+    [SerializeField]
+    private float movementSpeed;
+    private float jumpPower;
+    private bool fR;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         movementSpeed = 10f;
         rb = GetComponent<Rigidbody2D>();
+
+        jumpPower = 14f;
+        fR = true;
     }
- 
+    private bool IsGrounded()
+    {
+        return true;
+    }
+
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        vertical = Input.GetAxis("Vertical");
+        
         handMovement(horizontal);
     }
 
     void handMovement(float horizontal)
     {
+        rb.linearVelocity = new Vector2(horizontal * movementSpeed, rb.linearVelocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        flip(horizontal);
+        if (vertical > 0)
+        {
+            Jump();
+        }
+    }
+
+    void flip(float horizontal)
+    {
+        if (horizontal > 0 && !fR || horizontal < 0 && fR)
+        {
+            fR = !fR;
+            Vector2 tS = transform.localScale;
+
+            tS.x *= -1;
+
+            transform.localScale = tS;
+         }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocityY = vertical * jumpPower;
+    }
+
+    void OnCollisionEnter2D()
+    {
         
     }
 
+    void OnCollisionExit2D()
+    {
+
+    }
 }
